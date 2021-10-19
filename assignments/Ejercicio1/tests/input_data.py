32db5917c9085c17ebc97bac444c4befcf325e89
fabyuribe <78222449+fabyuribe@users.noreply.github.com>
2021-10-19 09:09:03 -0500

    Update input_data.py

--- assignments/Ejercicio1/tests/input_data.py
+++ assignments/Ejercicio1/tests/input_data.py
@@ -2,18 +2,17 @@
 
 # List of lists, where each of the inner lists has all the inputs for a single test
 input_values = [
-    # Test case 1
+     # Test case 1
     (
     [2, 3, 5, 18, 0, 21, -1, 7],
     ["Ingresa números positivos, para terminar de capturar ingresa un negativo",">>> ", ">>> ", ">>> ", 
-    ">>> ", ">>> ", ">>> ", ">>> ", "[2, 3, 5, 18, 0, 21]", "Mayores que: ",
-    "[2, 3, 5, 0]", "[18, 21]"],
+    ">>> ", ">>> ", ">>> ", ">>> ", "[2, 3, 5, 18, 0, 21]", "Mayores que: ", "[18, 21]", "[2, 3, 5, 0]"],
     ["La salida no cumple con el caso de prueba."]
     ),
     # Test case 2
     (
     ["-1", "-1"],
-    ["Ingresa números positivos, para terminar de capturar ingresa un negativo",">>> ","[]","Múltiplos de: ","Error"],
+    ["Ingresa números positivos, para terminar de capturar ingresa un negativo",">>> ","[]","Mayores que: ","Error"],
     ["Qué pasa si de entrada recibes un -1 y n no es un número positivo"]
     )
     ]
